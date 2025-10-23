dec84b14c69515ec6c004c639f179ba6c5184a0e
Ayden Tan <62932373+Devastator165@users.noreply.github.com>
2025-10-23 19:17:40 +0800

    Update mouseling.py

--- mouseling.py
+++ mouseling.py
@@ -1,1 +1,16 @@
 print("I am a cute mouse!")
+
+for i in range(5):
+    print("Squeak!")    
+
+
+def nibble():
+    print("Nibbling on some cheese!")
+
+while True:
+    decision = input("Do you want to nibble? (yes/no): ").strip().lower()
+    if decision == 'yes':
+        nibble()
+    elif decision == 'no':
+        print("Okay, maybe later!")
+        break
