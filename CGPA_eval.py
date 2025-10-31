8fc55e4121b89ed53874c24109cb3dc95de8d330
HoangNguyen12-rgb <nxhoang22@gmail.com>
2025-10-31 13:49:59 +0800

    Add files via upload

---
+++ CGPA_eval.py
@@ -0,0 +1,16 @@
+def Mean_of_CGPA(list):
+    sum = 0
+    for element in list:
+        sum += element
+    mean = sum/len(list)
+    return mean
+
+def Check_range(list):
+    mean = Mean_of_CGPA(list)
+    dev = mean / 10
+    while True:
+        for i in range(50):
+            if abs(list["G-1"][i]["cgpa"] - mean) <= dev:
+                return False
+            else: 
+                return True
