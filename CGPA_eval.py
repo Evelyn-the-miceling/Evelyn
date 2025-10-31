8eec3efecabb95fa83515bf874b12ab26f7393a4
HoangNguyen12-rgb <nxhoang22@gmail.com>
2025-10-31 13:51:47 +0800

    Update CGPA_eval.py

--- CGPA_eval.py
+++ CGPA_eval.py
@@ -11,8 +11,8 @@ def Check_range(list):
     while True:
         for i in range(50):
             if abs(list["G-1"][i]["cgpa"] - mean) <= dev: #Fix to fit with output of list
-                return False
-            else: 
-
                 return True
+            else: 
+                return False
+
 
